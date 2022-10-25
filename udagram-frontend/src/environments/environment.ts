2fced8e3ca7c8130ef4114f7f495ab01eea685f3
shine102 <nguyenngotunglam2@gmail.com>
2022-10-25 17:15:15 +0700

    fix apiHost frontend

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aa1c78d1e828e48be8693157f07377ac-1365205674.ap-southeast-1.elb.amazonaws.com/api/v0'
 };
 
 /*
